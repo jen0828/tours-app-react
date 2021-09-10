5c2681128ab9f4a6e7ec34ca768ea1c461c27347
Jenny Moon <jenny0moon@gmail.com>
2021-09-10 16:55:36 +0100

    Add: delete posts function

--- src/Tour.js
+++ src/Tour.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-const Tour = ({ id, image, info, price, name }) => {
+const Tour = ({ id, image, info, price, name, removeTour }) => {
   const [readMore, setReadMore] = useState(false)
   return (
     <article className="single-tour">
@@ -15,8 +15,7 @@ const Tour = ({ id, image, info, price, name }) => {
         <button onClick={() => setReadMore(!readMore)}>
           {readMore ? "show less" : "read more"}
         </button>
-
-        <button className="delete-btn">
+        <button className="delete-btn" onClick={() => removeTour(id)}>
           not interested
         </button>
       </footer>
